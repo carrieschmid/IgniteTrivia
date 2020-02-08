2ab8be297886b8d3254eb258ca9f4b42eb1146c6
Carrie Schmid <carriepederson7@gmail.com>
2020-02-08 09:55:38 -0800

    make api call

--- app/models/question-store/question-store.ts
+++ app/models/question-store/question-store.ts
@@ -1,5 +1,7 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
-import { QuestionModel } from "../question"
+import { QuestionModel, QuestionSnapshot, Question } from "../question"
+import { withEnvironment } from "../extensions"
+import { GetQuestionsResult } from "../../services/api"
 
 /**
  * Model description here for TypeScript hints.
@@ -10,15 +12,23 @@ export const QuestionStoreModel = types
     questions: types.optional(types.array(QuestionModel), []),
   })
   .views(self => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
-  .actions(self => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+  .actions(self => ({
+    saveQuestions: (questionSnapshots: QuestionSnapshot[]) => {
+      const questionModels: Question[] = questionSnapshots.map(c => QuestionModel.create(c)) // create model instances from the plain objects
+      self.questions.replace(questionModels) // Replace the existing data with the new data
+    },
+  }))
+  .actions(self => ({
+    getQuestions: flow(function*() {
+      const result: GetQuestionsResult = yield self.environment.api.getQuestions()
 
-/**
-  * Un-comment the following to omit model attributes from your snapshots (and from async storage).
-  * Useful for sensitive data like passwords, or transitive state like whether a modal is open.
-
-  * Note that you'll need to import `omit` from ramda, which is already included in the project!
-  *  .postProcessSnapshot(omit(["password", "socialSecurityNumber", "creditCardNumber"]))
-  */
+      if (result.kind === "ok") {
+        self.saveQuestions(result.questions)
+      } else {
+        __DEV__ && console.tron.log(result.kind)
+      }
+    }),
+  }))
 
 type QuestionStoreType = Instance<typeof QuestionStoreModel>
 export interface QuestionStore extends QuestionStoreType {}
