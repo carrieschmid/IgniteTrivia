7f119b266b2f9c80acc74a2ab7b062765e146a62
Carrie Schmid <carriepederson7@gmail.com>
2020-02-08 09:44:09 -0800

    add changes to project

---
+++ app/models/question-store/question-store.ts
@@ -0,0 +1,26 @@
+import { Instance, SnapshotOut, types } from "mobx-state-tree"
+import { QuestionModel } from "../question"
+
+/**
+ * Model description here for TypeScript hints.
+ */
+export const QuestionStoreModel = types
+  .model("QuestionStore")
+  .props({
+    questions: types.optional(types.array(QuestionModel), []),
+  })
+  .views(self => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+  .actions(self => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+
+/**
+  * Un-comment the following to omit model attributes from your snapshots (and from async storage).
+  * Useful for sensitive data like passwords, or transitive state like whether a modal is open.
+
+  * Note that you'll need to import `omit` from ramda, which is already included in the project!
+  *  .postProcessSnapshot(omit(["password", "socialSecurityNumber", "creditCardNumber"]))
+  */
+
+type QuestionStoreType = Instance<typeof QuestionStoreModel>
+export interface QuestionStore extends QuestionStoreType {}
+type QuestionStoreSnapshotType = SnapshotOut<typeof QuestionStoreModel>
+export interface QuestionStoreSnapshot extends QuestionStoreSnapshotType {}
--- app/services/api/api.ts
+++ app/services/api/api.ts
@@ -2,6 +2,27 @@ import { ApisauceInstance, create, ApiResponse } from "apisauce"
 import { getGeneralApiProblem } from "./api-problem"
 import { ApiConfig, DEFAULT_API_CONFIG } from "./api-config"
 import * as Types from "./api.types"
+import { QuestionSnapshot } from "../../models/question"
+import uuid from "react-native-uuid"
+import { decodeHTMLEntities } from "../../utils/html-decode"
+
+const API_PAGE_SIZE = 10
+
+const convertQuestion = (raw: any): QuestionSnapshot => {
+  const id = uuid.v1()
+  const decodedQuestion = decodeHTMLEntities(raw.question)
+  const decodedAnswers = raw.incorrect_answers.map(a => decodeHTMLEntities(a))
+
+  return {
+    id: id,
+    category: raw.category,
+    type: raw.type,
+    difficulty: raw.difficulty,
+    question: decodedQuestion,
+    correctAnswer: decodeHTMLEntities(raw.correct_answer),
+    incorrectAnswers: decodedAnswers,
+  }
+}
 
 /**
  * Manages all requests to the API.
@@ -45,42 +66,11 @@ export class Api {
   }
 
   /**
-   * Gets a list of users.
+   * Gets a list of trivia questions.
    */
-  async getUsers(): Promise<Types.GetUsersResult> {
+  async getQuestions(): Promise<Types.GetQuestionsResult> {
     // make the api call
-    const response: ApiResponse<any> = await this.apisauce.get(`/users`)
-
-    // the typical ways to die when calling an api
-    if (!response.ok) {
-      const problem = getGeneralApiProblem(response)
-      if (problem) return problem
-    }
-
-    const convertUser = raw => {
-      return {
-        id: raw.id,
-        name: raw.name,
-      }
-    }
-
-    // transform the data into the format we are expecting
-    try {
-      const rawUsers = response.data
-      const resultUsers: Types.User[] = rawUsers.map(convertUser)
-      return { kind: "ok", users: resultUsers }
-    } catch {
-      return { kind: "bad-data" }
-    }
-  }
-
-  /**
-   * Gets a single user by ID
-   */
-
-  async getUser(id: string): Promise<Types.GetUserResult> {
-    // make the api call
-    const response: ApiResponse<any> = await this.apisauce.get(`/users/${id}`)
+    const response: ApiResponse<any> = await this.apisauce.get("", { amount: API_PAGE_SIZE })
 
     // the typical ways to die when calling an api
     if (!response.ok) {
@@ -90,12 +80,11 @@ export class Api {
 
     // transform the data into the format we are expecting
     try {
-      const resultUser: Types.User = {
-        id: response.data.id,
-        name: response.data.name,
-      }
-      return { kind: "ok", user: resultUser }
-    } catch {
+      const rawQuestions = response.data.results
+      const convertedQuestions: QuestionSnapshot[] = rawQuestions.map(convertQuestion)
+      return { kind: "ok", questions: convertedQuestions }
+    } catch (e) {
+      __DEV__ && console.tron.log(e.message)
       return { kind: "bad-data" }
     }
   }
